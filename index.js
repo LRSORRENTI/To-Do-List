70a44b4936ad665ef5e2ea8c8946596e90ec93ee
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-28 18:00:59 -0500

    Added functionality for left click to complete todo item, right click with contextmenu to remove to do item

--- index.js
+++ index.js
@@ -3,5 +3,32 @@ const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
 form.addEventListener('submit', (event) => {
-     
+     event.preventDefault();
+     addToDo()
 })
+
+const addToDo = (todo) => {
+    let todoText = input.value;
+    if(todo){
+        todoText = todo.text;
+    }
+    if(todoText){
+        const todoEl = document.createElement('li');
+        if(todo && todo.completed){
+            todoEl.classList.add('completed');
+        }
+        todoEl.innerText = todoText;
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+        } )
+
+        todoEl.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            todoEl.remove()
+        } )
+
+
+        todosUL.appendChild(todoEl)
+        input.value = ''
+    }
+}
