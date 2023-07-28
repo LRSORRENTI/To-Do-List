f3351c395fe190bd39df77b2a50307cf847e8b34
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-28 18:34:52 -0500

    Added the update local storage function

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const todosUL = document.getElementById('todos');
 
 const todos = JSON.parse(localStorage.getItem('todos'))
 
-if(todos){
-    todos.forEach(item => {
-        addToDo(item)})
+// if(todos){
+//     todos.forEach(item => {
+//         addToDo(item)})
     
-} else {
-    throw new Error('No Todos')
-}
+// } else {
+//     throw new Error('No Todos')
+// }
 
-form.addEventListener('submit', (event) => {
-     event.preventDefault();
-     addToDo()
-})
+// form.addEventListener('submit', (event) => {
+//      event.preventDefault();
+//      addToDo()
+// })
 
 const addToDo = (todo) => {
     let todoText = input.value;
@@ -43,8 +43,34 @@ const addToDo = (todo) => {
 
         todosUL.appendChild(todoEl)
         input.value = ''
+
+        updateLocalStorage()
+
     }
+}
+
+function updateLocalStorage(){
+    todosEl = document.querySelectorAll('li')
+    const todos = [];
+    todosEl.forEach( todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
 
 // localStorage.setItem('name', JSON.stringify(obj))
 // JSON.parse(localStorage.getItem(obj))
+
+if(todos){
+    todos.forEach(item => {
+        addToDo(item)})
+    
+} 
+
+form.addEventListener('submit', (event) => {
+     event.preventDefault();
+     addToDo()
+})
